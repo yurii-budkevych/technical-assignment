d8317374efec82cf62b6427cd7b9c845cc78be10
yurii-budkevych <60780508+yurii-budkevych@users.noreply.github.com>
2020-02-12 14:28:35 +0100

    Clean up DemoApplicationTests

--- demo/src/test/java/com/technical/assignment/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/technical/assignment/demo/DemoApplicationTests.java
@@ -1,14 +1,11 @@
 package com.technical.assignment.demo;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -19,10 +16,6 @@ public class DemoApplicationTests {
 	private TestRestTemplate restTemplate;
 
 	@Test
-	public void hello() {
-//		ResponseEntity<String> entity = this.restTemplate.getForEntity("/service/get",
-//				String.class);
-//		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
-//		assertThat(entity.getBody()).isEqualTo("{\"Response\":\"get\",\"Hello\":\"Holidu\"}");
+	public void contextLoads() {
 	}
 }
